a1f126a974524beb2b122d2a5629ed1d3eb35a99
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2021-11-09 15:42:00 +0600

    Signatories page

--- src/Components/Signatories/PeopleDescription.js
+++ src/Components/Signatories/PeopleDescription.js
@@ -4,7 +4,7 @@ import './PeopleDescription.scss';
 export default function PeopleDescription(props) {
     const {full_name, known_for, image} = props;
     return (
-        <div className="col-lg-4 col-md-6 col-sm-6 people-description mt-4">
+        <div className="col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-6 people-description mt-4">
             <div className="row">
               <div className="col-md-6 img-fluid p-0">
                 <img
@@ -14,7 +14,7 @@ export default function PeopleDescription(props) {
                 />
               </div>
               <div className="col-lg-6 col-md-6 col-sm-4 p-0 d-flex flex-column justify-content-end people-description-data">
-              <div className="personality-box col-lg-6 col-md-6 col-sm-4 p-0 d-flex flex-column justify-content-end">
+              <div className="col-lg-6 col-md-6 col-sm-4 p-0 d-flex flex-column justify-content-end">
                 <p id="personality-name">{full_name}</p>
                 <p id="personality-status">{known_for}</p>
               </div>
